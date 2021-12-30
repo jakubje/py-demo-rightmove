77dc0daf3e044ba90101fd06a531027382b8ef4a
jakubje <jakubjeczalik@gmail.com>
2021-12-30 11:29:47 +0000

    Update

--- rightMove.py
+++ rightMove.py
@@ -20,13 +20,13 @@ class PropertyScraper():
         'Referer': 'https://www.rightmove.co.uk/property-to-rent/search.html?searchLocation=London&useLocationIdentifier=true&locationIdentifier=REGION%5E87490&rent=To+rent',
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
+        self.count = 0
         self.properties = list()
 
     def scrape(self):
-        totalPropertyCount = 0
-        count = 0
 
         while True:
+            print(self.count)
             try:
                 params = (
                     ('locationIdentifier', 'REGION^87490'),
@@ -34,7 +34,7 @@ class PropertyScraper():
                     ('maxBedrooms', '3'),
                     ('minBedrooms', '3'),
                     ('maxPrice', '3000'),
-                    ('index', count),
+                    ('index', self.count),
                     ('propertyTypes', ''),
                     ('includeLetAgreed', 'false'),
                     ('mustHave', ''),
@@ -44,32 +44,48 @@ class PropertyScraper():
                 )
                 
                 response = requests.get('https://www.rightmove.co.uk/property-to-rent/find.html', headers=self.headers, params=params)
-                soup = bs(response.text, 'html.parser')
-                properties = soup.find_all("div",{"class": "l-searchResult is-list"})
-                totalPropertyCount = soup.find("div",{"id": "searchHeader"}).text.split(" ")[0].replace(",", "")
+                if response.status_code == 200:
 
-                for property in properties:
-                    self.properties.append(
-                        {
-                            'id': property['id'].split("-")[1],
-                            'propertlyLink': "https://www.rightmove.co.uk/properties/{}".format(property['id'].split("-")[1]),
-                            'propertyImg' : property.find("img", {"alt": "Property Image 1"})['src'],
-                            'propertyType' : property.find("h2", {"class": "propertyCard-title"}).text.strip(),
-                            'propertyPrice' : property.find("span", {"class": "propertyCard-priceValue"}).text,
-                            'address' : property.find("address", {"class": "propertyCard-address"}).text.strip(),
-                            'addedDate' : property.find("div", {"class": "propertyCard-branchSummary"}).span.text,
-                            'contactNumber' : property.find("a", {"class": "propertyCard-contactsPhoneNumber"}).text
-                        }
-                    )
+                    self.count += 24
 
-                totalPageCountIndex = round(int(totalPropertyCount)/24) * 24
+                    soup = bs(response.text, 'html.parser')
+                    properties = soup.find_all("div",{"class": "l-searchResult is-list"})
+                    totalPropertyCount = soup.find("div",{"id": "searchHeader"}).text.split(" ")[0].replace(",", "")
 
-                count += 24
-                if count == 72:
-                    print(json.dumps(self.properties,indent=2,ensure_ascii=False))
+                    for property in properties:
+                        self.properties.append(
+                            {
+                                'id': property['id'].split("-")[1],
+                                'propertlyLink': "https://www.rightmove.co.uk/properties/{}".format(property['id'].split("-")[1]),
+                                'propertyImg' : property.find("img", {"alt": "Property Image 1"})['src'],
+                                'propertyType' : property.find("h2", {"class": "propertyCard-title"}).text.strip(),
+                                'propertyPrice' : property.find("span", {"class": "propertyCard-priceValue"}).text,
+                                'address' : property.find("address", {"class": "propertyCard-address"}).text.strip(),
+                                'addedDate' : property.find("div", {"class": "propertyCard-branchSummary"}).span.text,
+                                'contactNumber' : property.find("a", {"class": "propertyCard-contactsPhoneNumber"}).text
+                            }
+                        )
+
+                    totalPageCountIndex = round(int(totalPropertyCount)/24) * 24
+
+
+                    if self.count >= totalPageCountIndex:
+                        self.saveToJsonFile()
+                        break
+
+                elif response.status_code == 400:
+                    print("Page Not Found")
+                    self.saveToJsonFile()
                     break
+                
+
             except Exception as e:
                 print(e)
+
+    def saveToJsonFile(self):
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(self.properties, f, ensure_ascii=False, indent=4)
+
 
 scraper = PropertyScraper()
 scraper.scrape()
